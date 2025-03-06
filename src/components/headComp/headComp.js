4093bb932421f7a66409139f914bc6cb290618d8
krish736 <developers@whozzat.com>
2025-03-06 10:04:48 +0530

    293 | circular fav icon

--- src/components/headComp/headComp.js
+++ src/components/headComp/headComp.js
@@ -11,8 +11,8 @@ export default function HeadComp() {
                     ))}
                 <meta name="zoom-domain-verification" content="ZOOM_verify_122f4fb2b7734340b90fb0ae391bb1d0"></meta>
                 <link rel="apple-touch-icon" sizes="76x76" href="/assets/brand/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="96x96" href="/assets/brand/favicon-96x96.png" />
-                <link rel="icon" type="image/png" sizes="48x48" href="/assets/brand/favicon-48x48.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/assets/brand/favicon-circle-144x144.png" />
+                <link rel="icon" type="image/png" sizes="48x48" href="/assets/brand/favicon-circle-48x48.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/assets/brand/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/assets/brand/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
