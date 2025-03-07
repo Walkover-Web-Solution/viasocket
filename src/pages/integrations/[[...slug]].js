27eac5f9fad4dc036ed9158601e64ba44b6f7e2d
krish736 <developers@whozzat.com>
2025-03-07 17:59:24 +0530

    TEST

--- src/pages/integrations/[[...slug]].js
+++ src/pages/integrations/[[...slug]].js
@@ -254,6 +254,9 @@ export async function getServerSideProps(context) {
                 categories: categories || [],
                 blogData: blogData || [],
             },
+
+
+            
         };
     }
 }
