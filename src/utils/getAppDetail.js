308b89be528055668ede236498285668cb83b8d2
krish736 <developers@whozzat.com>
2025-04-09 10:39:44 +0530

    325 | mcp page

--- src/utils/getAppDetail.js
+++ src/utils/getAppDetail.js
@@ -2,5 +2,5 @@ export default function getAppDetails(combos, app) {
     if (!combos?.plugins || !combos.plugins[app]) {
         return null; // or handle the missing plugin case as needed
     }
-    return combos.plugins[app];
+    return combos?.plugins[app];
 }
