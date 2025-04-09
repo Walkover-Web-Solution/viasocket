d4ac2315c897d1e61aad4c5b7a9d01e82fbb3e10
krish736 <developers@whozzat.com>
2025-04-09 10:51:40 +0530

    test | hotfix

--- src/components/IntegrationsComp/integrationsAppOneComp/integrationsAppOneComp.js
+++ src/components/IntegrationsComp/integrationsAppOneComp/integrationsAppOneComp.js
@@ -15,7 +15,6 @@ import CombinationCardComp from '@/components/combinationCardComp/combinationCar
 import { setUtmSource } from '@/utils/handleUtmSource';
 import UseCaseList from '@/components/useCaseList/UseCaseList';
 import GetStarted from '@/components/getStarted/getStarted';
-import { setUtmSource } from '@/utils/handleUtmSource';
 
 export default function IntegrationsAppOneComp({
     appOneDetails,
