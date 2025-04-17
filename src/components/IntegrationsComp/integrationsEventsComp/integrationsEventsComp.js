0fab607e949b80021ce48798ed10399334e0b9f1
krish736 <developers@whozzat.com>
2025-04-17 14:06:58 +0530

    330-test | hotfix

--- src/components/IntegrationsComp/integrationsEventsComp/integrationsEventsComp.js
+++ src/components/IntegrationsComp/integrationsEventsComp/integrationsEventsComp.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MdAdd, MdAdsClick, MdCheck, MdClose, MdKeyboardArrowDown } from 'react-icons/md';
 import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
 import { setUtmInCookies, setUtmSource } from '@/utils/handleUtmSource';
