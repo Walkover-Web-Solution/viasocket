583dc8bc769057f09186443c985442ac67f64ca9
Viasocket Interns <developers@whozzat.com>
2025-05-05 10:59:50 +0530

    UI improvements in all page 1

--- src/pages/mcp/saas.js
+++ src/pages/mcp/saas.js
@@ -16,7 +16,7 @@ export default function pricing({ navData, footerData, faqData, metaData, blogDa
     return (
         <div className="container cont pb-4 lg:gap-20 md:gap-16 gap-12">
             <MetaHeadComp metaData={metaData} page={'/mcp'} />
-            <div className="container sticky top-0 z-[100]">
+            <div className="sticky top-0 z-[100]">
                 <Navbar navData={navData} utm={'/mcp'} />
             </div>
             <div className="container py-8">
@@ -24,7 +24,7 @@ export default function pricing({ navData, footerData, faqData, metaData, blogDa
 
                 <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 py-16">
                     <div className="cont gap-4 justify-center w-full text-center max-w-4xl">
-                        <h1 className="h1 ">
+                        <h1 className="h1 !uppercase !font-medium">
                             {/* Connect Your AI with<span className="text-accent"> 1,000+</span> MCPs */}
                             Launch Your Own MCP Server within Your SaaS
                         </h1>
