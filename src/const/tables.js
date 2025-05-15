74f300769dbf2f730b6a123e67649ef5f18e653e
Viasocket Interns <developers@whozzat.com>
2025-05-15 17:08:04 +0530

    Change Header content

--- src/const/tables.js
+++ src/const/tables.js
@@ -1,4 +1,5 @@
-export const NAVIGATION = 'tbl7lj8ev';
+// export const NAVIGATION = 'tbl7lj8ev';
+export const NAVIGATION = 'tblbdin9l';
 export const FOOTER = 'tbl6u2cba';
 export const METADATA = 'tbl2bk656';
 export const ALLFEATURES = 'tbl5p2jus';
