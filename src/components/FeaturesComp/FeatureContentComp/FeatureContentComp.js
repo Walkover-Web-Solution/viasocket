daef7d903325c834e4d2db31fd6330c8b9f0e9b8
Ragini Mahobiya <ragini@whozzat.com>
2025-05-26 14:09:53 +0530

    Add container

--- src/components/FeaturesComp/FeatureContentComp/FeatureContentComp.js
+++ src/components/FeaturesComp/FeatureContentComp/FeatureContentComp.js
@@ -1,15 +1,17 @@
 export default function FeatureContentComp({ featureData }) {
     if (featureData && featureData.length > 0)
         return (
-            <div className="container border custom-border p-12 bg-white flex flex-col gap-6">
-                {featureData?.map((faq, index) => {
-                    return (
-                        <ul className="flex flex-col gap-1" key={index} style={{listStyleType: 'square'}}>
-                            <li className="font-semibold text-lg">{faq?.question}</li>
-                            <li className="list-none">{faq?.answer}</li>
-                        </ul>
-                    );
-                })}
+            <div className="container">
+                <div className="border custom-border p-12 bg-white flex flex-col gap-6">
+                    {featureData?.map((faq, index) => {
+                        return (
+                            <ul className="flex flex-col gap-1" key={index} style={{ listStyleType: 'square' }}>
+                                <li className="font-semibold text-lg">{faq?.question}</li>
+                                <li className="list-none">{faq?.answer}</li>
+                            </ul>
+                        );
+                    })}
+                </div>
             </div>
         );
 }
