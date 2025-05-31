b2c50868673a973a88202a8ed9e3e99a4b335d37
krish736 <krishnakantdave02@gmail.com>
2025-05-31 15:12:48 +0530

    fix

--- src/utils/SendErrorMessage.js
+++ src/utils/SendErrorMessage.js
@@ -1,6 +1,7 @@
 export async function sendErrorMessage({ error }) {
+    error = error?.response?.data || error.message;
     try {
-        const response = await fetch(`https://flow.sokt.io/func/scrixVwRkMy0?error=${error}`);
+        const response = await fetch(`https://flow.sokt.io/func/scrixVwRkMy0?error=${JSON.stringify(error)}`);
         console.error(response);
     } catch (error) {
         console.error('Error', error);
