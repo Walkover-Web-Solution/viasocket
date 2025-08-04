8352419002acd9cd2c37bedb2335b068c4fe4708
krish736 <krishnakantdave02@gmail.com>
2025-08-04 12:57:20 +0530

    link redirection fix and open flow.viasocket links in new tab

--- src/utils/handleRedirection.js
+++ src/utils/handleRedirection.js
@@ -1,8 +1,14 @@
 import { setUtmSource } from './handleUtmSource';
 
-export const handleRedirect = (e, url) => {
+export const handleRedirect = (e, url, router) => {
     e.preventDefault();
     const source = typeof window !== 'undefined' ? window.location.pathname : '';
-    const utmParams = setUtmSource({ source: source });
-    window.location.href = `${url}state=${utmParams}`;
+    const utmParams = setUtmSource({ source });
+    const finalUrl = `${url}state=${utmParams}`;
+
+    if (router && url.startsWith('/')) {
+        router.push(finalUrl);
+    } else {
+        window.open(finalUrl, '_blank');
+    }
 };
