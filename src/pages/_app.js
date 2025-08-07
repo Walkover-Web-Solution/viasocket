99acdb871a424a9557c797ededef64a308004dfa
krish736 <krishnakantdave02@gmail.com>
2025-08-07 16:43:25 +0530

    add chat widget

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -127,6 +127,7 @@ export default function MyApp({ Component, pageProps, pagesData }) {
     return (
         <>
             <HeadComp canonicalUrl={canonicalUrl} />
+            <ChatWidget />
             {showSkeleton ? (
                 <Skeleton />
             ) : (
