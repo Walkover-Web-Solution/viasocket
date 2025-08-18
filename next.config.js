af002ddbb7cdfdbfa54ce0ed97804bf6593ac6b7
tanish <tanish@whozzat.com>
2025-08-18 12:09:30 +0530

    testing

--- next.config.js
+++ next.config.js
@@ -1,35 +1,41 @@
-module.exports = {
-    // experimental: {
-    //     runtime: 'experimental-edge',
-    //   },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    
     images: {
-        unoptimized: true,
-        loader: 'akamai',
-        path: '',
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-        ],
+      unoptimized: true,
+      loader: 'akamai',
+      path: '',
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'placehold.co',
+        },
+      ],
     },
-
+  
+    experimental: {
+      serverComponentsExternalPackages: [],
+    },
+  
     async redirects() {
-        return [
-            {
-                source: '/login',
-                destination: 'https://flow.viasocket.com/',
-                permanent: false,
-            },
-        ];
+      return [
+        {
+          source: '/login',
+          destination: 'https://flow.viasocket.com/',
+          permanent: false,
+        },
+      ];
     },
-
+  
     webpack(config) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            fs: false,
-        };
-
-        return config;
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+  
+      return config;
     },
-};
+  };
+  
+  export default nextConfig;
+  
