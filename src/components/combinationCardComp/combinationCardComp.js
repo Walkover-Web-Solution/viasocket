497803abfbe56d034a65de774d6a7094e0242f8c
Tanaykumar01 <tanayjhunjhunwala.official@gmail.com>
2025-09-25 12:40:54 +0530

    solved some bugs like text overflow and irregular size of Image Container

--- src/components/combinationCardComp/combinationCardComp.js
+++ src/components/combinationCardComp/combinationCardComp.js
@@ -11,12 +11,12 @@ export default function CombinationCardComp({ trigger, action, description, link
             <div className="cont w-full">
                 <div className="flex gap-8"></div>
                 <div className="flex items-center">
-                    <div className="border custom-border p-2 cont items-center group-hover:border-white">
-                        <Image alt='trigger icon' src={trigger?.iconurl} width={40} height={40} />
+                    <div className="border custom-border p-2 cont items-center bg-white" style={{ width: 60, height: 60, position: 'relative' }}>
+                        <Image alt='trigger icon' src={trigger?.iconurl} fill style={{ objectFit: 'contain' , padding: 6}}/>
                     </div>
-                    <div className="w-16 border-t-2 custom-border group-hover:border-white" />
-                    <div className="border custom-border p-2 cont items-center group-hover:border-white">
-                        <Image alt='action icon' src={action?.iconurl} width={40} height={40} />
+                    <div className="w-16 border-t-2 custom-border bg-white" />
+                    <div className="border custom-border p-2 cont items-center bg-white" style={{ width: 60, height: 60, position: 'relative' }}>
+                        <Image alt='action icon' src={action?.iconurl} fill style={{ objectFit: 'contain' , padding: 6}} />
                     </div>
                 </div>
             </div>
