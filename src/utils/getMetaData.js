b7ac2aeb6f8789bacc83fbd8f75d31f9d1df61bb
Diksha Jain <jdiksha394@gmail.com>
2025-11-14 17:20:55 +0530

    black friday sales deals page ui changes, added marquee component

--- src/utils/getMetaData.js
+++ src/utils/getMetaData.js
@@ -1,8 +1,9 @@
 import { getMeta } from './axiosCalls';
 
 export async function getMetaData(name, pageUrl) {
-    const allMetaData = await getMeta(pageUrl);
-
-    const matchedMeta = allMetaData.find((obj) => obj.name === name);
+    const allMetaData = (await getMeta(pageUrl)) || [];
+    const matchedMeta = Array.isArray(allMetaData)
+        ? allMetaData.find((obj) => obj?.name === name)
+        : null;
     return matchedMeta || null;
 }
