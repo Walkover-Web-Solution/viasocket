69c7d1fa4e0b702cf848ce046e7431867de010f8
Ragini Mahobiya <raginimahobiya07@gmail.com>
2025-12-22 10:55:01 +0530

    CTA buttons added to homepage with Start for free and Book a demo links. Start for free button links to /signup page with accent styling. Book a demo button links to cal.id scheduling page with outline styling and opens in new tab. Buttons positioned in centered flex container with gap spacing below BuildOptionsCTA component. | 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -125,6 +125,19 @@ const Index = ({
                     />
 
                     <BuildOptionsCTA />
+
+                    <div className="flex gap-4 justify-center mt-5">
+                        <Link href="/signup" className="btn btn-accent relative z-index-1">
+                            Start for free
+                        </Link>
+                        <Link
+                            href="https://cal.id/team/viasocket/workflow-setup-discussion"
+                            target="_blank"
+                            className="btn btn-outline relative z-index-1"
+                        >
+                            Book a demo
+                        </Link>
+                    </div>
                 </div>
             </div>
 
