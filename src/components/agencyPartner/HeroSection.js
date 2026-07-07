35aca3b8b67852981d12e87bb40adfcfd39baaa5
Ragini Mahobiya <raginimahobiya07@gmail.com>
2026-07-07 11:51:37 +0530

    Update "Talk to Sales" link in HeroSection to external calendar booking URL
    - Change support page link to direct Cal.com booking link for agency partnership
    - Add target="_blank" to open calendar in new tab || 2

--- src/components/agencyPartner/HeroSection.js
+++ src/components/agencyPartner/HeroSection.js
@@ -31,7 +31,7 @@ const HeroSection = ({ appCount }) => {
                     <Link href="#pricing" className="btn btn-accent">
                         Become a Partner
                     </Link>
-                    <Link href="/support" className="btn btn-outline">
+                    <Link href="https://cal.id/divyansh-jain/agency-partnership" className="btn btn-outline" target="_blank">
                         Talk to Sales
                         <ArrowRight size={16} className="transform group-hover:translate-x-1 transition-transform" />
                     </Link>
