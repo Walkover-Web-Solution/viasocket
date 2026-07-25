a29af3b1eb1fac5f9d0fc8db8570c3a561ad2eaa
Ragini Mahobiya <raginimahobiya07@gmail.com>
2026-07-25 12:50:41 +0530

    Remove A/B testing variants from hero section and simplify redirect utility
    - Remove HeroSectionB and HeroSectionC imports and HeroVariant component from HeroContainer
    - Remove variant prop from HeroContainer and always render default HeroSection
    - Remove variant-based background class logic, always use dotted-background
    - Remove getVariant import and variant prop passing in HomePage
    - Remove extraParams parameter from handleRedirect utility
    - Remove automatic import=true parameter for flow.viasocket.com URLs || 1

--- src/app/components/home/HeroContainer.js
+++ src/app/components/home/HeroContainer.js
@@ -2,24 +2,10 @@
 
 import { useCallback, useState } from 'react';
 import HeroSection from './HeroSection';
-import HeroSectionB from './HeroSectionB';
-import HeroSectionC from './HeroSectionC';
 import ShowAppsIndexOptimized from './ShowAppsIndexOptimized';
 import SearchAndResults from './SearchAndResults';
 
-// A/B hero variants keyed off the `variant` cookie assigned server-side (middleware).
-function HeroVariant({ variant, appCount, hasToken }) {
-    switch (variant) {
-        case 'B':
-            return <HeroSectionB appCount={appCount} hasToken={hasToken} />;
-        case 'C':
-            return <HeroSectionC appCount={appCount} hasToken={hasToken} />;
-        default:
-            return <HeroSection appCount={appCount} hasToken={hasToken} />;
-    }
-}
-
-export default function HeroContainer({ appCount, initialApps, templateData, hasToken, variant }) {
+export default function HeroContainer({ appCount, initialApps, templateData, hasToken }) {
     const [hasActiveSearch, setHasActiveSearch] = useState(false);
 
     // Handle search state changes from SearchAndResults component
@@ -28,12 +14,11 @@ export default function HeroContainer({ appCount, initialApps, templateData, has
     }, []);
 
     const containerClasses = 'min-h-0 pt-24';
-    const bgClass = variant === 'C' ? '' : 'dotted-background';
 
     return (
-        <div className={`${containerClasses} ${bgClass} px-4 mx-auto relative global-top-space`}>
+        <div className={`${containerClasses} px-4 mx-auto relative global-top-space dotted-background`}>
             <div className="text-center">
-                <HeroVariant variant={variant} appCount={appCount} hasToken={hasToken} />
+                <HeroSection appCount={appCount} hasToken={hasToken} />
 
                 <ShowAppsIndexOptimized isHomePage apps={initialApps} appCount={appCount} />
 
